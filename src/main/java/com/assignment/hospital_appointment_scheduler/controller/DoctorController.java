a366facdd890ce43f91b910bd4ff5994e639b909
KishanVermma <kishanverma7786@gmail.com>
2026-02-26 20:18:30 +0530

    Updated CORS for Netlify frontend

--- src/main/java/com/assignment/hospital_appointment_scheduler/controller/DoctorController.java
+++ src/main/java/com/assignment/hospital_appointment_scheduler/controller/DoctorController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://symbdemo.netlify.app")
 @RequiredArgsConstructor
 public class DoctorController {
     @Autowired
